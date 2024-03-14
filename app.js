bda9fb52be38a8feac6d5e2b7b515bbd89b72e83
Cyano Prem <66469412+cyanoprem@users.noreply.github.com>
2024-03-14 22:00:51 +0530

    learnt data types

--- app.js
+++ app.js
@@ -4,10 +4,23 @@ const myName = "Prem"
 
 let num = 5
 
-console.log(myName) // Prem
-console.log(num) // 5
+// console.log(myName) // Prem
+// console.log(num) // 5
 
 num = 10
 
-console.log(num) // 10
+// console.log(num) // 10
+
+
+
+// Data types
+
+let myNumber;
+console.log(myNumber) // undefined
+
+let boolValue = true
+let nullValue = null
+
+console.log(boolValue)
+console.log(nullValue)
 
