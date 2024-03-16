3758403510646d163abad7d3c814d9d8b5a4a084
Cyano Prem <66469412+cyanoprem@users.noreply.github.com>
2024-03-16 21:20:24 +0530

    Array

--- app.js
+++ app.js
@@ -16,11 +16,23 @@ num = 10
 // Data types
 
 let myNumber;
-console.log(myNumber) // undefined
+// console.log(myNumber) // undefined
 
 let boolValue = true
 let nullValue = null
 
-console.log(boolValue)
-console.log(nullValue)
+// console.log(boolValue)
+// console.log(nullValue)
+
+//  Array
+
+let arr = [1, 20, "John", "good"]
+// console.log(arr)
+
+let intergerArray = [2, 5, 7, 9, 11] 
+
+console.log(intergerArray.length)
+console.log(intergerArray[0])
+console.log(intergerArray[1])
+console.log(intergerArray[intergerArray.length-1])
 
